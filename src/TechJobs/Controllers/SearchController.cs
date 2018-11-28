7ff77355ed10e69bc41955981ccee5f021db875c
Ellen Green <eegreen.95@gmail.com>
2018-11-27 22:12:27 -0600

    initial commit

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -13,7 +13,28 @@ namespace TechJobs.Controllers
             return View();
         }
 
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+            List<Dictionary<string, string>> jobs = new List<Dictionary<string, string>>();
+            if (searchType.Equals("all"))
+            {
+                jobs = JobData.FindByValue(searchTerm);
+            }
+
+            else
+            {
+                jobs = JobData.FindByColumnAndValue(searchType, searchTerm); 
+            }
+
+            ViewBag.columns = ListController.columnChoices;
+            ViewBag.jobs = jobs;
+     
+            return View("Index");
+        }
+
         // TODO #1 - Create a Results action method to process 
+            //parameters- type of search, search term
+                //named based on corresponding form field names
         // search request and display results
 
     }
